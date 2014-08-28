e3a37237f7e2a5495fc19f34e43babc899f47614
kinduff <abarcadabra@gmail.com>
2014-08-28 14:58:04 -0300

    Adds admin forms for affiliates

--- app/models/spree/affiliate.rb
+++ app/models/spree/affiliate.rb
@@ -1,6 +1,9 @@
 module Spree
   class Affiliate < Spree::Base
     has_many :referred_records
+    validates :name, presence: true
+    validates :path, presence: true
+    validates :partial, presence: true
 
     def referred_users
       referred_records.includes(:user).collect(&:user).compact
